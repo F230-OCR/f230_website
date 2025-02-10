f1113392ad77c2d473fd74a357251c64c9b84e4b
Rares08 <raresanghel2008@gmail.com>
2025-02-10 23:04:15 +0200

    Update app.py to run in debug mode and change default port to 8080

--- app.py
+++ app.py
@@ -183,5 +183,4 @@ def index():
     return render_template('index.html', info=None)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
